422a3fee0c466f5da7cf785defcd4fe4af7ff057
AngelloCortesPalomares <cortes.palomares.angello00@gmail.com>
2025-10-15 21:35:21 -0500

    Cuerpo del programa para el punto 1

---
+++ src/main/java/Modelo/Competidor.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Angello
+ */
+public class Competidor {
+    
+    private String nombre;
+    private int edad;
+    private String pais;
+    private int rankingMundial;
+    private double estatura;
+    private double peso;
+    private int puntos;
+    
+}
---
+++ src/main/java/Modelo/Equipo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import java.util.List;
+
+/**
+ *
+ * @author Angello
+ */
+public class Equipo {
+    
+    private String nombre;
+    private String pais;
+    private List<Competidor> competidores;
+    
+}
