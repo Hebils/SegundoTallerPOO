063f4a4fea13935eecdeeca0852d4dd24ed4c001
AngelloCortesPalomares <cortes.palomares.angello00@gmail.com>
2025-10-17 11:17:24 -0500

    Modificacion de la clase competidor para poder registrar al competidor y posicionarlo en el ranking

--- src/main/java/Modelo/Competidor.java
+++ src/main/java/Modelo/Competidor.java
@@ -18,4 +18,101 @@ public class Competidor {
     private double peso;
     private int puntos;
     
+    public Competidor(String nombre, int edad, String pais, int rankingMundial, double estatura, double peso, int puntos) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.pais = pais;
+        this.rankingMundial = rankingMundial;
+        this.estatura = estatura;
+        this.peso = peso;
+        this.puntos = puntos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getRankingMundial() {
+        return rankingMundial;
+    }
+
+    public void setRankingMundial(int rankingMundial) {
+        this.rankingMundial = rankingMundial;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+    
+    public void actualizarRanking(int nuevoRanking) {
+        this.rankingMundial = nuevoRanking;
+    }
+    
+    public int ranking(int puntosObtenidos) {
+
+        int ajusteRanking = puntosObtenidos / 10;
+        
+        int nuevoRanking = this.rankingMundial - ajusteRanking;
+        
+        this.rankingMundial = Math.max(1, nuevoRanking);
+        
+        return this.rankingMundial;
+    }
+    
+    public String obtenerDatos() {
+        return "Competidor: " + nombre + " | País: " + pais + " | Edad: " + edad +
+               " | Ranking Mundial: " + rankingMundial + " | Puntos: " + puntos;
+    }
+    
+    @Override
+    public String toString() {
+        return "Datos del Competidor\n\n" +
+               "Nombre: " + nombre + "\n" +
+               "Edad: " + edad + " años\n" +
+               "País: " + pais + "\n" +
+               "Ranking Mundial: " + rankingMundial + "\n" +
+               "Estatura: " + estatura + " m\n" +
+               "Peso: " + peso + " kg\n" +
+               "Puntos Acumulados: " + puntos + "\n";
+    }
 }
