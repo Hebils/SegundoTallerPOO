0cb735a233ee47aa168c86943ef0741800192dba
AngelloCortesPalomares <cortes.palomares.angello00@gmail.com>
2025-10-17 19:26:29 -0500

    Modificacion de la clase competencia para realizar el registro de todas

--- src/main/java/Modelo/Competencia.java
+++ src/main/java/Modelo/Competencia.java
@@ -4,6 +4,7 @@
  */
 package Modelo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,7 +18,7 @@ public class Competencia {
 
     public Competencia(String nombreEvento, List<Equipo> equipos) {
         this.nombreEvento = nombreEvento;
-        this.equipos = equipos;
+        this.equipos = new ArrayList<>();
     }
 
     public String getNombreEvento() {
@@ -36,5 +37,27 @@ public class Competencia {
         this.equipos = equipos;
     }
     
+    public void agregarEquipo(Equipo e) {
+        this.equipos.add(e);
+    }
+    
+    public String generarReporte() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("==================================================\n");
+        sb.append("REPORTE DE COMPETENCIA: ").append(nombreEvento).append("\n");
+        sb.append("==================================================\n");
+        sb.append("Total de equipos inscritos: ").append(equipos.size()).append("\n\n");
+
+        for (Equipo equipo : equipos) {
+            sb.append(equipo.obtenerDatosEquipo()).append("\n");
+        }
+
+        return sb.toString();
+    }
+    
+    @Override
+    public String toString() {
+        return "Competencia [Evento: " + nombreEvento + ", Equipos: " + equipos.size() + "]";
+    }
     
 }
